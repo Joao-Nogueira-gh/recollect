bf74cb1eeb11bef69d231d6437a49e7769fdb6a7
nogas <joaonogueira20@ua.pt>
2020-05-07 22:11:55 +0100

    started frontend kinda not rly

---
+++ ReCollect/src/main/java/ua/tqs/ReCollect/ReCollectController.java
@@ -0,0 +1,31 @@
+package ua.tqs.ReCollect;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JsonParser;
+import org.springframework.boot.json.JsonParserFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ReCollectController {
+
+    //@Autowired
+    //private Service service;
+
+	@GetMapping("/home")
+	public String index(Model model) {
+		
+		return "index";
+	}
+    
+}
