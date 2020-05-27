f34669a68e5299fde03049e8738ddffc3a1a252b
Alexandre Lopes <alexandrejflopes@ua.pt>
2020-05-27 14:09:23 +0100

    changes item description max length in form

--- ReCollect/src/main/java/ua/tqs/ReCollect/utils/ItemForm.java
+++ ReCollect/src/main/java/ua/tqs/ReCollect/utils/ItemForm.java
@@ -18,7 +18,7 @@ public class ItemForm {
     private Categories categoria;
 
     @NotNull
-    @Size(max = 2500) // TODO: check this with DB
+    @Size(max = 255) // TODO: check this with DB
     private String descricao;
 
     @NotNull
